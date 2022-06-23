d450bcd96e3648f7d8f76c7222f42356e9675ee8
tonyxuan2021 <tonyyexuan@gmail.com>
2022-06-22 22:37:23 -0700

    fix image present issue

--- src/components/Trending.js
+++ src/components/Trending.js
@@ -1,5 +1,5 @@
 import React from "react";
-import test from "../assets/thumbnails/beyond-earth/regular/small.jpg"
+// import test from "../assets/thumbnails/beyond-earth/regular/small.jpg"
 
 const Trending = ({ newArr }) => {
   console.log(newArr);
@@ -10,18 +10,20 @@ const Trending = ({ newArr }) => {
 
   return (
     <div>
-    <h3>Trending</h3>
-      {newArr.map((movie) => {
-        const { year, category, rating, title, thumbnail } = movie;
-        console.log(thumbnail.regular.small)
-        return (
-            <div>
-            <img src={thumbnail.regular.small}></img>
-            {/* <img src={test}></img> */}
-            <p>{title}</p>
+      <h3>Trending</h3>
+      <div className="trending__movie__wrapper">
+        {newArr.map((movie, index) => {
+          const { year, category, rating, title, thumbnail } = movie;
+          console.log(thumbnail.regular.small);
+          return (
+            <div key={index}>
+              <img src={`${thumbnail.regular.small}`}></img>
+              {/* <img src={test}></img> */}
+              <p>{title}</p>
             </div>
-        )
-      })}
+          );
+        })}
+      </div>
     </div>
   );
 };
